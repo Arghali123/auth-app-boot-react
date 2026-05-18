ef3a872779320fa02eca90e4d4c06d85bea1f9aa
Denish Arghali <150317382+Arghali123@users.noreply.github.com>
2026-05-18 14:28:26 +0545

    started working on frontend

--- auth-frontend/src/main.tsx
+++ auth-frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import RootLayout from "./pages/RootLayout.tsx";
+import App from "./App.tsx";
+import Login from "./pages/Login.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<App />} />
+        <Route path="/login" element={<Login/>}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+);
